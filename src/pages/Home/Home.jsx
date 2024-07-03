4ad4af18da3d8327b3d9fd45d995a83d096831ce
Tymur Rezaei <rezai.timur.04@gmail.com>
2024-07-03 11:31:56 -0700

    setup routes

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Body from '../../components/Body/Body'
+
+function Home() {
+  return (
+    <>
+        <Header></Header>
+        <Body></Body>
+    </>
+  )
+}
+
+export default Home
