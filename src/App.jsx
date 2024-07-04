0e2f42f363d3aef5f14f35b886cb897f05982273
Tymur Rezaei <rezai.timur.04@gmail.com>
2024-07-03 17:01:34 -0700

    api called on frontend

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import Calendar from './modals/Calendar'
+import Details from './pages/Details/Details'
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      
+
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
       <Route path="/calendar" element={<Calendar/>} />
+      <Route path="/details" element={<Details/>} />
       
     </Routes>
     </BrowserRouter>
---
+++ src/pages/Details/Details.jsx
@@ -0,0 +1,36 @@
+import { React, useState, useEffect } from 'react'
+import axios from 'axios'
+
+function Details() {
+    const [weatherData, setWeatherData] = useState([""]);
+    const [UvData, setUvData] = useState(0);
+
+    async function getWeather() {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${import.meta.env.VITE_BC_VAN_LAT}&lon=${import.meta.env.VITE_BC_VAN_LON}&units=metric&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
+        setWeatherData(response.data);
+    }
+
+    async function getUv() {
+        const response = await axios.get("https://currentuvindex.com/api/v1/uvi?latitude=49.2608724&longitude=-123.113952");
+        setUvData(response.data.now.uvi);
+    }
+
+    useEffect(() => {
+        getWeather();
+        getUv();
+    }, [])
+
+
+
+  return (
+    <>
+        <div>Details</div>
+        <br></br>
+        <p>{weatherData.name}</p>
+        <br></br>
+        <p>{UvData}</p>
+    </>
+  )
+}
+
+export default Details
